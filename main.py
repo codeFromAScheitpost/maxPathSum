d5429f3516369680f7d1a31e6f78e7fbc682f645
codeFromAScheitpost <95752991+codeFromAScheitpost@users.noreply.github.com>
2024-08-11 14:01:09 +0200

    implement algorithm

---
+++ main.py
@@ -0,0 +1,57 @@
+import sys
+
+
+def max_sum(triangle: list[list[int]]):
+    """
+    Computes the max path sum for a triangle.
+
+    :param triangle: list representation of a triangle with the list at triangle[0] has length 1 and for all i length(triangle[i])+1=length(triangle[i+1])
+    :return: value of the max path sum
+    """
+    # each iteration the triangle is shrunk by one line
+    while len(triangle) != 1:
+
+        last_line: list[int] = triangle.pop(len(triangle) - 1)
+        second_to_last_line: list[int] = triangle.pop(len(triangle) - 1)
+        for i in range(0, len(second_to_last_line)):
+            second_to_last_line[i] += max(last_line[i], last_line[i + 1])
+        triangle.append(second_to_last_line)
+
+    return triangle[0][0]
+
+
+def read_file_to_triangle(file_path: str):
+    """
+    Parses a file and turns it into a list representation of a triangle.
+
+    :param file_path: the path of a file that represents a triangle. each line of the triangle is divided by \n each valule is seperated by a whitespace
+    :return: the list representation of the triangle
+    """
+    with open(file_path, "r") as file:
+        whole_file: str = file.read()
+    lines = whole_file.splitlines()
+    lines = list(map(lambda x: x.split(' '), lines))
+    triangle: list[list[int]] = list(map(lambda x: list(map(lambda y: int(y), x)), lines))
+
+    # checks if the input triangle is well-formed.
+    for i in range(0, len(triangle) - 2):
+        if len(triangle[i]) + 1 != len(triangle[i + 1]):
+            raise ValueError("the input is malformed, not each line is followed by a line that is one element larger")
+
+    return triangle
+
+
+if __name__ == '__main__':
+    # to run part one use 1 as input, to run part two use 2.
+    # the input was chosen because the exercise specified only those two problems a file input would be possible as well
+    if len(sys.argv) < 2:
+        part_nr = 1
+    else:
+        part_nr = int(sys.argv[1])
+
+    if part_nr < 1 or part_nr > 2:
+        raise IndexError("input should be 1 or 2")
+
+    exercise_files = ["ex01.txt", "triangle.txt"]
+    read_triangle = read_file_to_triangle(exercise_files[part_nr - 1])
+    print("the length of the maximal path for part ", part_nr, "is: ", max_sum(read_triangle))
